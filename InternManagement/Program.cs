b20ad59258e85bf0b26c4e931550991ec1d06355
longSE03 <longndse170223@fpt.edu.vn>
2024-05-27 18:35:27 +0700

    Change Connect

--- InternManagement/Program.cs
+++ InternManagement/Program.cs
@@ -1,3 +1,6 @@
+using InternManagement.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<InternManagementContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
 
 var app = builder.Build();
 
